6645119069765863ff10282d8158b18bb19a1e49
Kamil Górski <kamil.gorski@siili.com>
2017-03-23 09:28:16 +0100

    Live lane recognition working

--- task.py
+++ task.py
@@ -3,119 +3,98 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 import math
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
 
 def grayscale(img):
-    """Applies the Grayscale transform
-    This will return an image with only one color channel
-    but NOTE: to see the returned image as grayscale
-    (assuming your grayscaled image is called 'gray')
-    you should call plt.imshow(gray, cmap='gray')"""
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # Or use BGR2GRAY if you read an image with cv2.imread()
-    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
 def canny(img, low_threshold, high_threshold):
-    """Applies the Canny transform"""
     return cv2.Canny(img, low_threshold, high_threshold)
 
 def gaussian_blur(img, kernel_size):
-    """Applies a Gaussian Noise kernel"""
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
 
 def region_of_interest(img, vertices):
-    """
-    Applies an image mask.
-    
-    Only keeps the region of the image defined by the polygon
-    formed from `vertices`. The rest of the image is set to black.
-    """
-    #defining a blank mask to start with
     mask = np.zeros_like(img)
-       
-    
-    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
     if len(img.shape) > 2:
         channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
         ignore_mask_color = (255,) * channel_count
     else:
         ignore_mask_color = 255
-        
-    #filling pixels inside the polygon defined by "vertices" with the fill color    
+
     cv2.fillPoly(mask, vertices, ignore_mask_color)
-    #returning the image only where mask pixels are nonzero
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
 
-def draw_lines(img, lines, color=[255, 0, 0], thickness=5):
-    """
-    NOTE: this is the function you might want to use as a starting point once you want to 
-    average/extrapolate the line segments you detect to map out the full
-    extent of the lane (going from the result shown in raw-lines-example.mp4
-    to that shown in P1_example.mp4).  
-    
-    Think about things like separating line segments by their 
-    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-    line vs. the right line.  Then, you can average the position of each of 
-    the lines and extrapolate to the top and bottom of the lane.
-    
-    This function draws `lines` with `color` and `thickness`.    
-    Lines are drawn on the image inplace (mutates the image).
-    If you want to make the lines semi-transparent, think about combining
-    this function with the weighted_img() function below
-    """
+def draw_lines(img, lines, color=[255, 0, 0], thickness=10):
     for line in lines:
         for x1,y1,x2,y2 in line:
             cv2.line(img, (x1, y1), (x2, y2), color, thickness)
 
-def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
-    """
-    `img` should be the output of a Canny transform.
-        
-    Returns an image with hough lines drawn.
-    """
+lprev = [0, 0]
+rprev = [0, 0]
+lcoefmean = 1
+rcoefmean = 1
+def extention_lines(img, rho, theta, threshold, min_line_len, max_line_gap, region):
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
     left_line = [[], []]
     right_line = [[],[]]
 
+    global lprev, rprev, lcoefmean, rcoefmean
+
     for line in lines:
         coef = (line[0][3] - line[0][1]) / (line[0][2] / line[0][0])
         if coef < 0:
-            left_line[0].extend(line[0][1::2])
-            left_line[1].extend(line[0][::2])
+            lcoefmean += coef
+            lcoefmean /= 2
+            if (coef / lcoefmean) > 0.2: 
+                left_line[1].extend(line[0][1::2])
+                left_line[0].extend(line[0][::2])
         else:
-            right_line[0].extend(line[0][1::2])
-            right_line[1].extend(line[0][::2])
+            rcoefmean += coef
+            rcoefmean /= 2
+            if (coef / rcoefmean) > 0.2: 
+                right_line[1].extend(line[0][1::2])
+                right_line[0].extend(line[0][::2])
 
+    l = np.polyfit(left_line[0], left_line[1], 1)
+    l = np.mean(np.array([lprev, l]), axis=0)
+    lprev = l
+    l = np.poly1d(l)
 
-    print(left_line)
-    print(right_line)
+    r = np.polyfit(right_line[0], right_line[1], 1)
+    r = np.mean(np.array([rprev, r]), axis=0)
+    rprev = r
+    r = np.poly1d(r)   
+
+    lines = []
+    lines.append([region[0][0][0], int(l(region[0][0][0])), region[0][3][0], int(l(region[0][3][0]))])
+    lines.append([region[0][1][0], int(r(region[0][1][0])), region[0][2][0], int(r(region[0][2][0]))])
+
     line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-    draw_lines(line_img, lines)
+    draw_lines(line_img, [lines])
     return line_img
 
-# Python 3 has support for cool math symbols.
-
 def weighted_img(img, initial_img, a=0.8, b=1., g=0.):
-    """
-    `img` is the output of the hough_lines(), An image with lines drawn on it.
-    Should be a blank image (all black) with lines drawn on it.
-    
-    `initial_img` should be the image before any processing.
-    
-    The result image is computed as follow
-    NOTE: initial_img and img must be the same shape!
-    """
     return cv2.addWeighted(initial_img, a, img, b, g)
 
-original = mpimg.imread('test_images/solidWhiteRight.jpg')
-image = grayscale(original)
-image = gaussian_blur(image, 7)
-image = canny(image, 30, 80)
-region = np.array( [[[415,330],[545,330],[900,535],[120,535]]], dtype=np.int32 )
-image = region_of_interest(image, region)
-image = hough_lines(image, 1, 0.03, 3, 10, 10)
-image = weighted_img(image, original)
+def process_image(image):
+    result = grayscale(image)
+    result = gaussian_blur(result, 7)
+    result = canny(result, 100, 200)
+    region = np.array( [[[415,330],[545,330],[900,535],[120,535]]], dtype=np.int32 )
+    result = region_of_interest(result, region)
+    result = extention_lines(result, 1, 0.03, 10, 20, 3, region)
+    result = weighted_img(result, image)    
+    return result
 
-plt.imshow(image)
-plt.show()
+# white_output = 'test_videos_output/solidWhiteRight.mp4'
+# clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4")
+white_output = 'test_videos_output/solidYellowLeft.mp4'
+clip1 = VideoFileClip("test_videos/solidYellowLeft.mp4")
+# white_output = 'test_videos_output/challenge.mp4'
+# clip1 = VideoFileClip("test_videos/challenge.mp4")
+white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+white_clip.write_videofile(white_output, audio=False)
